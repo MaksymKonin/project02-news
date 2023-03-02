c3d4a2024d5889869494c9d811b297613e90c7f6
Oleksandr_Moroz <110786733+Kontrabas888@users.noreply.github.com>
2023-03-02 23:16:36 +0200

    read-capture-apdate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
 import createCalendar from "./js/renderCalendar";
 
 createCalendar();
-
-import renderCard from "./js/markupCard";
-
-import apiNews from './js/newsApiService';
-console.log(1);
-
-async function search() {
-    const response = await apiNews.searchNews('html');
-    renderCard(response.response.docs)
-  console.log(response.response.docs);
-}
-search();
