c3e1dd02d1df8def5d63220ea07f0f9449a840ef
KyryloCA <3dprofix@gmail.com>
2023-03-03 07:25:19 -0500

    2

--- src/js/renderWeather.js
+++ src/js/renderWeather.js
@@ -1,64 +1,134 @@
 import {
-  getCurrentWeather,
   getDailyWeather,
   getDemoWeather,
+  getDetailLocation,
 } from './weatherApiService';
 
-function insertWeatherBlock(wetherButtonState, weatherInsertionPoint) {
-  // console.log('rendering...');
-  // console.log('rendering...', wetherButtonState);
-  // console.log('rendering...', weatherInsertionPoint);
+function secondsToString(seconds) {
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const date = new Date(seconds);
+  const dayOfWeek = days[date.getDay()];
+  const dayOfMonth = date.getDate();
+  const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  const monthName = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+  return {
+    dayOfWeek: dayOfWeek,
+    dayOfMonth: dayOfMonth,
+    monthName: monthName,
+    year: year,
+  };
+  // return `<div>${dayOfWeek}</div><div>${dayOfMonth} ${monthName} ${year}</div>`;
+}
+
+function createDateString(obj) {
+  return `<div>${obj.dayOfWeek}</div><div>${obj.dayOfMonth} ${obj.monthName} ${obj.year}</div>`;
+}
+
+function createForecastDateString(obj) {
+  return `<span>${obj.dayOfWeek},${obj.dayOfMonth} ${obj.monthName} </span>`;
+}
+
+function buildWeatherMarkup(
+  weatherInsertionPoint,
+  parsedData,
+  weatherTimestamp
+) {
+  const getTimeObj = secondsToString(weatherTimestamp);
+  const timeString = createDateString(getTimeObj);
+
+  const string = `<div style="background-color: yellow; width: 150px; height: 300px;">
+  <p><span class="weathere-city-name">${parsedData.sityName}</span></p>
+  <p><span class="weathere-temperature">${parsedData.temperature}&#176C</span></p>
+  <p><span class="weathere-description">${parsedData.description}</span></p>
+  <img class="weathere-icon"src="http://openweathermap.org/img/wn/${parsedData.iconCode}@2x.png" alt="Weather icon" >
+  ${timeString}
+<button class="weather-button">weather for week</button></div>`;
+
+  weatherInsertionPoint.insertAdjacentHTML('beforeend', string);
+}
+
+function buildForecastMarkup(weatherInsertionPoint, query1, query2) {
+  const daysString = query1.daily
+    .map(value => {
+      return `<li><span>${createForecastDateString(
+        secondsToString(value.dt)
+      )}     ${Math.round(value.temp.min)} / ${Math.round(
+        value.temp.max
+      )}   <img style="height: 30px;" class="weathere-icon"src="http://openweathermap.org/img/wn/${
+        value.weather[0].icon
+      }@2x.png" alt="Weather icon" ></span> </li>`;
+    })
+    .join('');
+  const string = `<div style="background-color: blue; width: 150px; height: 300px;"><p>forecast for ${query2[0].name}</p><ul>${daysString}</ul><button class="weather-button">weather for today</button></div>`;
+
+  weatherInsertionPoint.insertAdjacentHTML('beforeend', string);
+}
+
+async function insertWeatherBlock(
+  wetherButtonState,
+  weatherInsertionPoint,
+  actualLocation,
+  weatherTimestamp
+) {
+  //set default data obj
+  let parsedData = {
+    sityName: 'Timbuktu',
+    temperature: '36.6',
+    description: 'Always hot',
+    iconCode: '01d',
+    maxTemp: 100,
+    minTemp: -100,
+  };
 
   if (wetherButtonState === true) {
     //single req
-    console.log('single');
-    const resultArray = getDemoWeather();
-    resultArray.then(r => {
-      console.log(r);
-      console.log(r.current);
-      console.log(r.current.weather[0]);
-      console.log('------------------------------');
-      console.log('area name:', r.timezone);
-      console.log('temperature', r.current.temp);
-      console.log('description:', r.current.weather[0].main);
-      console.log('icon:', r.current.weather[0].icon);
+    const rawData = getDemoWeather(actualLocation); //request is here
+    const sityNameData = getDetailLocation(actualLocation); //detail location
+    const a = Promise.all([rawData, sityNameData])
+      .then(value => {
+        return value;
+      })
+      .catch(error => console.log(error));
+
+    a.then(result => {
+      const [query1, query2] = result;
+      parsedData.temperature = query1.current.temp;
+      parsedData.description = query1.current.weather[0].main;
+      parsedData.iconCode = query1.current.weather[0].icon;
+      parsedData.sityName = query2[0].name;
+      buildWeatherMarkup(weatherInsertionPoint, parsedData, weatherTimestamp);
     });
   } else {
     //multiple req
-    console.log('multiple');
+
+    const rawData = getDailyWeather(actualLocation); //request is here
+    const sityNameData = getDetailLocation(actualLocation); //detail location
+    const a = Promise.all([rawData, sityNameData])
+      .then(value => {
+        return value;
+      })
+      .catch(error => console.log(error));
+
+    a.then(result => {
+      const [query1, query2] = result;
+
+      buildForecastMarkup(weatherInsertionPoint, query1, query2);
+    });
   }
 }
-
-// async function performQuery(wetherButtonState) {
-//   try {
-//     const geoPosition = await getCurrentPosition();
-//     console.log('async result is', geoPosition);
-//     return geoPosition;
-//   } catch (error) {}
-// }
-
-// async function getCurrentPosition() {
-//   return navigator.geolocation.getCurrentPosition(
-//     successPosition,
-//     alternativeGeo
-//   );
-// }
-
-// // const requestResult = getCurrentPosition();
-
-// // console.log('результат', requestResult);
-
-// function alternativeGeo(error) {
-//   console.log('ошибка здесь', error);
-//   const a = error;
-//   return a;
-// }
-// function successPosition(position) {
-//   console.log('успех', position);
-//   // collectPosition(position.coords.latitude, position.coords.longitude);
-//   const a = position;
-//   return a;
-// }
-// console.log('script continue');
 
 export { insertWeatherBlock };
--- src/js/weatherApiService.js
+++ src/js/weatherApiService.js
@@ -1,54 +1,33 @@
 const WEATHER_API_KEY = '19831cf247bc3b5af5cfecb7de987016';
 const WEATHER_SOURCE = 'https://api.openweathermap.org/data/3.0/onecall';
-const userLatitude = 39.8865;
-const userLongitude = -83.4483;
+const WEATHER_REVERSE = 'http://api.openweathermap.org/geo/1.0/reverse';
 
 import axios from 'axios';
 // import { getPosition } from './locationApiService.js';
 
 const queryOptions = {
   key: WEATHER_API_KEY,
-  latitude: userLatitude,
-  longitude: userLongitude,
   dailyOption: 'current,minutely,hourly,alerts',
   currentOption: 'minutely,hourly,daily,alerts',
   units: 'metric',
 };
 
-async function getCurrentWeather(position) {
-  const colectedURL = `${WEATHER_SOURCE}?lat=${position.coords.latitude}&lon=${position.coords.longitude}&exclude=${queryOptions.currentOption}&appid=${queryOptions.key}&units=${queryOptions.units}`;
+async function getDailyWeather({ lat, lon }) {
+  const weatherURL = `${WEATHER_SOURCE}?lat=${lat}&lon=${lon}&exclude=${queryOptions.dailyOption}&units=${queryOptions.units}&appid=${queryOptions.key}`;
 
-  return axios.get(colectedURL);
+  return axios.get(weatherURL).then(r => r.data);
 }
 
-async function getDailyWeather(lat, lon) {
-  const colectedURL = `${WEATHER_SOURCE}?lat=${queryOptions.latitude}&lon=${queryOptions.longitude}&exclude=${queryOptions.dailyOption}&appid=${queryOptions.key}`;
+async function getDemoWeather({ lat, lon }) {
+  const weatherURL = `${WEATHER_SOURCE}?lat=${lat}&lon=${lon}&exclude=${queryOptions.currentOption}&units=${queryOptions.units}&appid=${queryOptions.key}`;
 
-  return axios.get(colectedURL);
+  return axios.get(weatherURL).then(r => r.data);
 }
 
-async function getDemoWeather() {
-  const colectedURL = `${WEATHER_SOURCE}?lat=${queryOptions.latitude}&lon=${queryOptions.longitude}&exclude=${queryOptions.currentOption}&units=${queryOptions.units}&appid=${queryOptions.key}`;
-  return axios.get(colectedURL).then(r => r.data);
+async function getDetailLocation({ lat, lon }) {
+  const weatherURL = `${WEATHER_REVERSE}?lat=${lat}&lon=${lon}&limit=1&appid=${queryOptions.key}`;
+
+  return axios.get(weatherURL).then(r => r.data);
 }
 
-export { getCurrentWeather, getDailyWeather, getDemoWeather };
-
-// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-// function collectPosition() {
-//   navigator.geolocation.getCurrentPosition(r => {
-//     const obj = { lat: r.coords.latitude, lon: r.coords.longitude };
-//     console.log('this is obj', obj);
-//   });
-// }
-// export { collectPosition, getCurrentPosition };
-
-// function getCurrentPosition() {
-//   return new Promise((resolve, reject) => {
-//     navigator.geolocation.getCurrentPosition(
-//       position => resolve(position),
-//       error => reject(error)
-//     );
-//   });
-// }
+export { getDailyWeather, getDemoWeather, getDetailLocation };
