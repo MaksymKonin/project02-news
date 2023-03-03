2abe2a048132bee5708b08ccf2c720243803e9e2
KyryloCA <3dprofix@gmail.com>
2023-03-03 11:04:10 -0500

    weather beta

--- src/js/renderWeather.js
+++ src/js/renderWeather.js
@@ -1,38 +1,4 @@
-import {
-  getDailyWeather,
-  getDemoWeather,
-  getDetailLocation,
-} from './weatherApiService';
-
-function secondsToString(seconds) {
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const date = new Date(seconds);
-  const dayOfWeek = days[date.getDay()];
-  const dayOfMonth = date.getDate();
-  const monthNames = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'June',
-    'July',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
-  const monthName = monthNames[date.getMonth()];
-  const year = date.getFullYear();
-  return {
-    dayOfWeek: dayOfWeek,
-    dayOfMonth: dayOfMonth,
-    monthName: monthName,
-    year: year,
-  };
-  // return `<div>${dayOfWeek}</div><div>${dayOfMonth} ${monthName} ${year}</div>`;
-}
+import { secondsToString } from './weatherServiceMain.js';
 
 function createDateString(obj) {
   return `<div>${obj.dayOfWeek}</div><div>${obj.dayOfMonth} ${obj.monthName} ${obj.year}</div>`;
@@ -50,7 +16,7 @@ function buildWeatherMarkup(
   const getTimeObj = secondsToString(weatherTimestamp);
   const timeString = createDateString(getTimeObj);
 
-  const string = `<div style="background-color: yellow; width: 150px; height: 300px;">
+  const string = `<div class="weather__card__container">
   <p><span class="weathere-city-name">${parsedData.sityName}</span></p>
   <p><span class="weathere-temperature">${parsedData.temperature}&#176C</span></p>
   <p><span class="weathere-description">${parsedData.description}</span></p>
@@ -73,62 +39,14 @@ function buildForecastMarkup(weatherInsertionPoint, query1, query2) {
       }@2x.png" alt="Weather icon" ></span> </li>`;
     })
     .join('');
-  const string = `<div style="background-color: blue; width: 150px; height: 300px;"><p>forecast for ${query2[0].name}</p><ul>${daysString}</ul><button class="weather-button">weather for today</button></div>`;
+  const string = `<div class="weather__card__container"><p>forecast for ${query2[0].name}</p><ul>${daysString}</ul><button class="weather-button">weather for today</button></div>`;
 
   weatherInsertionPoint.insertAdjacentHTML('beforeend', string);
 }
 
-async function insertWeatherBlock(
-  wetherButtonState,
-  weatherInsertionPoint,
-  actualLocation,
-  weatherTimestamp
-) {
-  //set default data obj
-  let parsedData = {
-    sityName: 'Timbuktu',
-    temperature: '36.6',
-    description: 'Always hot',
-    iconCode: '01d',
-    maxTemp: 100,
-    minTemp: -100,
-  };
-
-  if (wetherButtonState === true) {
-    //single req
-    const rawData = getDemoWeather(actualLocation); //request is here
-    const sityNameData = getDetailLocation(actualLocation); //detail location
-    const a = Promise.all([rawData, sityNameData])
-      .then(value => {
-        return value;
-      })
-      .catch(error => console.log(error));
-
-    a.then(result => {
-      const [query1, query2] = result;
-      parsedData.temperature = query1.current.temp;
-      parsedData.description = query1.current.weather[0].main;
-      parsedData.iconCode = query1.current.weather[0].icon;
-      parsedData.sityName = query2[0].name;
-      buildWeatherMarkup(weatherInsertionPoint, parsedData, weatherTimestamp);
-    });
-  } else {
-    //multiple req
-
-    const rawData = getDailyWeather(actualLocation); //request is here
-    const sityNameData = getDetailLocation(actualLocation); //detail location
-    const a = Promise.all([rawData, sityNameData])
-      .then(value => {
-        return value;
-      })
-      .catch(error => console.log(error));
-
-    a.then(result => {
-      const [query1, query2] = result;
-
-      buildForecastMarkup(weatherInsertionPoint, query1, query2);
-    });
-  }
-}
-
-export { insertWeatherBlock };
+export {
+  createDateString,
+  createForecastDateString,
+  buildWeatherMarkup,
+  buildForecastMarkup,
+};
--- src/js/weatherServiceMain.js
+++ src/js/weatherServiceMain.js
@@ -1,12 +1,115 @@
 //import weather module:
-import { insertWeatherBlock } from './renderWeather';
+import {
+  createDateString,
+  createForecastDateString,
+  buildWeatherMarkup,
+  buildForecastMarkup,
+} from './renderWeather';
+
+import {
+  getDailyWeather,
+  getDemoWeather,
+  getDetailLocation,
+} from './weatherApiService';
 
 //basic weather setup
 let wetherButtonState = true;
 let weatherInsertionPoint = document.querySelector('.weather__card');
+let weatherButtonInsertionPoint = null;
+let weatherButtonEventListener = null;
+// console.log(weatherButtonInsertionPoint);
 let actualLocation = { lat: 39.8865, lon: -83.4483 };
 const currentTimestamp = new Date();
 let weatherTimestamp = currentTimestamp.getTime();
+
+//+++Imported from render++++++++++++++++++++++++++++++
+
+function secondsToString(seconds) {
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const date = new Date(seconds);
+  const dayOfWeek = days[date.getDay()];
+  const dayOfMonth = date.getDate();
+  const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+  const monthName = monthNames[date.getMonth()];
+  const year = date.getFullYear();
+  return {
+    dayOfWeek: dayOfWeek,
+    dayOfMonth: dayOfMonth,
+    monthName: monthName,
+    year: year,
+  };
+  // return `<div>${dayOfWeek}</div><div>${dayOfMonth} ${monthName} ${year}</div>`;
+}
+
+async function insertWeatherBlock(
+  wetherButtonState,
+  weatherInsertionPoint,
+  actualLocation,
+  weatherTimestamp
+) {
+  //set default data obj
+  let parsedData = {
+    sityName: 'Timbuktu',
+    temperature: '36.6',
+    description: 'Always hot',
+    iconCode: '01d',
+    maxTemp: 100,
+    minTemp: -100,
+  };
+
+  if (wetherButtonState === true) {
+    //single req
+    const rawData = getDemoWeather(actualLocation); //request is here
+    const sityNameData = getDetailLocation(actualLocation); //detail location
+    const a = Promise.all([rawData, sityNameData])
+      .then(value => {
+        return value;
+      })
+      .catch(error => console.log(error));
+
+    a.then(result => {
+      const [query1, query2] = result;
+      parsedData.temperature = query1.current.temp;
+      parsedData.description = query1.current.weather[0].main;
+      parsedData.iconCode = query1.current.weather[0].icon;
+      parsedData.sityName = query2[0].name;
+      buildWeatherMarkup(weatherInsertionPoint, parsedData, weatherTimestamp);
+    });
+  } else {
+    //multiple req
+
+    const rawData = getDailyWeather(actualLocation); //request is here
+    const sityNameData = getDetailLocation(actualLocation); //detail location
+    const a = Promise.all([rawData, sityNameData])
+      .then(value => {
+        return value;
+      })
+      .catch(error => console.log(error));
+
+    a.then(result => {
+      const [query1, query2] = result;
+
+      buildForecastMarkup(weatherInsertionPoint, query1, query2);
+    });
+  }
+}
+
+export { insertWeatherBlock };
+
+//+++END+++++
 
 //weather user consent (popup location prompt)
 async function userPositionConsent() {
@@ -15,7 +118,10 @@ async function userPositionConsent() {
     actualLocation.lat = r.coords.latitude;
     actualLocation.lon = r.coords.longitude;
     weatherTimestamp = r.timestamp;
+
     weatherInsertionPoint.innerHTML = ''; //clear the default weather block
+    // weatherButtonInsertionPoint = document.querySelector('.weather-button');
+    // weatherButtonEventListener
 
     insertWeatherBlock(
       wetherButtonState,
@@ -62,8 +168,4 @@ function weatherMarkup() {
   );
 }
 
-//+++Imported from render++++++++++++++++++++++++++++++
-
-//+++END+++++
-
-export { userPositionConsent, weatherMarkup };
+export { userPositionConsent, weatherMarkup, secondsToString };
