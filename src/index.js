248988603c935da551122dfa6c439de80ff197a1
JuliMelnikova <ulia682082@gmail.com>
2023-03-04 16:53:25 +0200

    add theme

--- src/index.js
+++ src/index.js
@@ -7,3 +7,63 @@ createCalendar();
 weatherMarkup();
 //run weather according to location
 userPositionConsent();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Theme switch
+const theme = {
+    light: 'light-theme',
+    dark: 'dark-theme',
+};
+
+const storageKey = 'themeKey';
+
+const checkBox = document.querySelector('.theme-switch__toggle');
+const body = document.querySelector('body');
+
+checkBox.addEventListener('change', onChange);
+isTheme();
+
+function onChange(ev) {
+    if (ev.target.checked) {
+        body.classList.remove('ligth-theme');
+        body.classList.add('dark-theme');
+        localStorage.setItem(storageKey, JSON.stringify(theme.dark));
+    } else {
+        body.classList.remove('dark-theme');
+        body.classList.add('ligth-theme');
+        localStorage.setItem(storageKey, JSON.stringify(theme.light));
+    }
+}
+
+function isTheme() {
+    const saveTheme = localStorage.getItem(storageKey);
+    if (!saveTheme) {
+        body.classList.add('ligth-theme');
+        localStorage.setItem(storageKey, JSON.stringify(theme.light));
+    } else {
+        const parseTheme = JSON.parse(saveTheme);
+        if (parseTheme === 'dark-theme') {
+            body.classList.add('dark-theme');
+            checkBox.checked = true;
+        }
+    }
+}
+
