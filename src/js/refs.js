4b58de26c88fd291ad351ec3ca5cba2b5868116b
MaksymKonin <skam707@gmail.com>
2023-03-04 19:32:16 +0200

    search виправив опечатку

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,5 @@
 export const refs = {
-  formEl: document.querySelector('.js-form-serch'),
+  formEl: document.querySelector('.js-form-search'),
   containerCategoriesEl: document.querySelector('.js-categories'),
   containerCardEl: document.querySelector('.js-container-card'),
   containerPaginationEl: document.querySelector('.js-pagination'),
