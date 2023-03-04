e45793f3e0b98dab76aa5bb06830e166a33c3d12
ESM <111918644+HoneATCS@users.noreply.github.com>
2023-03-04 23:49:33 +0200

    murkupCategory

--- src/js/renderCategories.js
+++ src/js/renderCategories.js
@@ -1,31 +1,23 @@
 import axios from "axios";
 
-
 const API_KEY = 'H4EzmbJjzcMKAQjlOxUvVd6TipG3GhzM';
-const CATEGORIES_LIST_URL = 'https://api.nytimes.com/svc/news/v3/content/section-list.json'
+const CATEGORIES_LIST_URL =   'https://api.nytimes.com/svc/news/v3/content/section-list.json';
 
-getcategoryNews()
+
+getlistCategories()
 {
-    return fetch(`${CATEGORIES_NEWS_URL}/${this.selectedСategories}.json?api-key=${API_KEY}`).then(responce => {
-        if (!responce.ok) { throw new Error(responce.statusText); } return responce.json()
-    })
+    return fetch(`${CATEGORIES_LIST_URL}?api-key=${API_KEY}`)
+        .then(responce => {
+            if (!responce.ok)
+            {
+                throw new Error(responce.statusText);
+            } return responce.json();
+        });
 }
 
-
-/* Слухач подій */
-function myFunction() {
-  document.getElementById("listCategory").classList.toggle("show");
-}
-// Закриття закриваючого списку, якщо клікнути за межами списку.
-const onclick = function(event) {
-  if (!event.target.matches('.btn')) {
-    const dropdowns = document.getElementsByClassName("categories");
-    i;
-    for (i = 0; i < dropdowns.length; i++) {
-      const openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
+const murkupMainCategory = document.querySelector('.btn-categories');
+const otherCategory = document.querySelector('.btn-others');
+getlistCategories.fetchCategories();
+async function renderCategoryMurkup() {
+    const categorArr= await getlistCategories.fetchCategories();
 }
