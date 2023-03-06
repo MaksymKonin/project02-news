2ef2fcecf4608f873f58ae9065a1696df916dfd8
Oleksandr_Moroz <110786733+Kontrabas888@users.noreply.github.com>
2023-03-06 22:30:51 +0200

    card-part-7

--- src/js/markupCard.js
+++ src/js/markupCard.js
@@ -11,17 +11,20 @@ export default function markupCard(card) {
   } = card;
   return `
     <li data-id-news ="${id_news}" class="js-card">
-      <div class="button-type">
+      <div class="use-text-tape">
         <span class="status-reed">Have read</span>
         <span class="job-text">"${section}"</span>
-        <button class="status-favorite" type="button">${statusFavorite}</button>
+        <button class="status-favorite add-status add-status-js" type="button">${statusFavorite}</button>
+        <button class="status-favorite remove-status remove-status-js" type="button">Remove from favorite</button>
       <div>
+      <div class="card-img-wrapper">
       <img class="card-img" src="${photo_url}" alt="photo news" />
+      </div>
       <a href="${page_url}"><h2 class="page-url">${title}</h2></a>
       <p class="abstract-text">${abstract}</p>
       <div>
         <span class="published-date">${published_date}</span>
-        <a class="read-more" href="${page_url}">Read more</a>
+        <a class="read-more" target="_blank" href="${page_url}">Read more</a>
       </div>
     </li>
     `;
