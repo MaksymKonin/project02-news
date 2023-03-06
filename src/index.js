cf182247eccb1cad48c6da0f055d4aede794f269
Oleksandr_Moroz <110786733+Kontrabas888@users.noreply.github.com>
2023-03-06 20:46:26 +0200

    card-part-5

--- src/index.js
+++ src/index.js
@@ -16,6 +16,12 @@ import localStorage from './js/localStorage';
 
 const newsApiService = new NewsApiService();
 
+const HAVE_READ_ID = "have-read-id" 
+const FAVORITES_NEWS = "favorite-news"
+let haveReadArray = [];
+let favoritesNewsArray = [];
+
+
 changeTheme();
 createCalendar();
 //---render categories---
@@ -63,6 +69,12 @@ async function searchNews() {
   }
   let normalizedData = normalaizData(response.response.docs);
   renderNews(normalizedData);
+
+  saveHaveReadNews()
+
+  saveFavoriteNews(normalizedData)
+
+  removeFavoriteNews(normalizedData)
   // } catch (err) {
   //   Notify.failure('Sorry, an error occurred, try again later');
   // }
@@ -77,6 +89,15 @@ async function createpopularNews() {
     }
     let normalizedData = normalaizData(response.results);
     renderNews(normalizedData);
+
+    setDefaultParams(normalizedData)  
+
+    saveHaveReadNews()
+
+    saveFavoriteNews(normalizedData)
+
+    removeFavoriteNews(normalizedData)
+
   } catch (err) {
     Notify.failure('Sorry, an error occurred, try again later');
   }
@@ -91,6 +112,11 @@ async function createNewsCategory() {
     }
     let normalizedData = normalaizData(response.results);
     renderNews(normalizedData);
+
+    saveHaveReadNews(normalizedData)
+
+    removeFavoriteNews(normalizedData)
+
   } catch (err) {
     Notify.failure('Sorry, an error occurred, try again later');
   }
@@ -121,4 +147,114 @@ async function createListCategories() {
 // свибір категорій/тестово
 function selectedСategories() {
   newsApiService.selectedСategories = 'automobiles, arts';
+}
+
+function saveHaveReadNews() {
+  const readMoreButtons = document.querySelectorAll(".read-more");
+    let readMoreArray = Array.from(readMoreButtons)
+    
+    readMoreArray.map(readMoreLink => {
+      readMoreLink.addEventListener("click", event => {
+        const parentLi = event.target.parentNode.parentNode.parentNode.parentNode;
+        const parentLiId = parentLi.dataset.idNews;
+        const isIncludeId = haveReadArray.includes(parentLiId)
+        if (!isIncludeId) {
+          haveReadArray.push(parentLiId)
+          localStorage.save(HAVE_READ_ID, JSON.stringify(haveReadArray))
+        }
+      })
+    })
+}
+
+function saveFavoriteNews(normalizedData) {
+  const addFavoriteButtons = document.querySelectorAll(".add-status-js");
+
+  let favoriteButtonsArray = Array.from(addFavoriteButtons)
+  
+  favoriteButtonsArray.map(addButtonHTML => {
+    addButtonHTML.addEventListener("click", event => {
+      const addButton = event.target 
+      const removeButton = addButton.nextElementSibling
+      const parentLi = addButton.parentNode.parentNode;
+      const parentLiId = parentLi.dataset.idNews;
+
+      addButton.style.display = "none"
+      removeButton.style.display = "block"
+
+      normalizedData.map(element => {
+        if (String(element.id_news) === parentLiId) {
+          favoritesNewsArray.push(element)
+          localStorage.save(FAVORITES_NEWS, JSON.stringify(favoritesNewsArray))
+          
+        }
+      })
+    })
+  })
+}
+
+function removeFavoriteNews(normalizedData) {
+  const removeFavoriteButtons = document.querySelectorAll(".remove-status-js");
+
+  let removeFavoriteButtonsArray = Array.from(removeFavoriteButtons)
+  
+  removeFavoriteButtonsArray.map(removeButtonHTML => {
+    removeButtonHTML.addEventListener("click", event => {
+      const removeButton = event.target 
+      const addButton = removeButton.previousElementSibling
+      const parentLi = removeButton.parentNode.parentNode;
+      const parentLiId = parentLi.dataset.idNews;
+
+      addButton.style.display = "block"
+      removeButton.style.display = "none"
+
+      normalizedData.map(element => {
+        if (String(element.id_news) === parentLiId) {
+          const index = favoritesNewsArray.indexOf(element)
+          favoritesNewsArray.splice(index, 1)
+          localStorage.save(FAVORITES_NEWS, JSON.stringify(favoritesNewsArray))
+          
+        }
+      })
+    })
+  })
+}
+
+function setDefaultParams(normalizedData) {
+  const favoriteNews = localStorage.load(FAVORITES_NEWS)
+  if (favoriteNews) {
+    const favoriteNewsParse = JSON.parse(favoriteNews)
+    
+    normalizedData.map(element => {
+      const test = favoriteNewsParse.includes(element)
+      // if (favoriteNewsParse)
+      // if (String(element.id_news) === parentLiId) {
+      //   const index = favoritesNewsArray.indexOf(element)
+      //   favoritesNewsArray.splice(index, 1)
+      //   localStorage.save(FAVORITES_NEWS, JSON.stringify(favoritesNewsArray))
+        
+      // }
+    })
+  }
+  
+  
+  // removeFavoriteButtonsArray.map(removeButtonHTML => {
+  //   removeButtonHTML.addEventListener("click", event => {
+  //     const removeButton = event.target 
+  //     const addButton = removeButton.previousElementSibling
+  //     const parentLi = removeButton.parentNode.parentNode;
+  //     const parentLiId = parentLi.dataset.idNews;
+
+  //     addButton.style.display = "block"
+  //     removeButton.style.display = "none"
+
+  //     normalizedData.map(element => {
+  //       if (String(element.id_news) === parentLiId) {
+  //         const index = favoritesNewsArray.indexOf(element)
+  //         favoritesNewsArray.splice(index, 1)
+  //         localStorage.save(FAVORITES_NEWS, JSON.stringify(favoritesNewsArray))
+          
+  //       }
+  //     })
+  //   })
+  // })
 }
--- src/js/markupCard.js
+++ src/js/markupCard.js
@@ -14,14 +14,17 @@ export default function markupCard(card) {
       <div class="button-type">
         <span class="status-reed">Have read</span>
         <span class="job-text">"${section}"</span>
-        <button class="status-favorite" type="button">${statusFavorite}</button>
+        <button class="status-favorite add-status add-status-js" type="button">${statusFavorite}</button>
+        <button class="status-favorite remove-status remove-status-js" type="button">Remove from favorite</button>
       <div>
+      <div class="card-img-wrapper">
       <img class="card-img" src="${photo_url}" alt="photo news" />
+      </div>
       <a href="${page_url}"><h2 class="page-url">${title}</h2></a>
       <p class="abstract-text">${abstract}</p>
       <div>
         <span class="published-date">${published_date}</span>
-        <a class="read-more" href="${page_url}">Read more</a>
+        <a class="read-more" target="_blank" href="${page_url}">Read more</a>
       </div>
     </li>
     `;
