87b09d40673615f668204d6588cdbb2448b6dfb5
Oksana Fylypchak <107584846+Kseniya1417@users.noreply.github.com>
2023-03-08 18:42:06 +0100

    render read news

--- src/js/pages/read.js
+++ src/js/pages/read.js
@@ -1,81 +1,21 @@
 import LocalStorageService from '../localStorage';
 import setNewActiveLink from '../currentLink';
 import changeTheme from '../changeTheme';
+import { createCardNotFound } from '../renderNews';
+import markupCard from '../markupCard';
+
 const localStorageService = new LocalStorageService();
 setNewActiveLink();
 changeTheme();
 
-const newsTest = [
-  {
-    id: 1,
-    title: 'News 1 title',
-    description: 'News 1 description',
-    imgUrl:
-      'https://cdn.cnn.com/cnnnext/dam/assets/230302094821-05-mv-gemini-cruise-ship-large-tease.jpg',
-    createdDate: '2023-03-01T16:17:30.814Z',
-    lastReadDate: '2023-03-02T16:17:30.814Z',
-  },
-  {
-    id: 2,
-    title: 'News 2 title',
-    description: 'News 2 description',
-    imgUrl:
-      'https://cdn.cnn.com/cnnnext/dam/assets/230302094821-05-mv-gemini-cruise-ship-large-tease.jpg',
-    createdDate: '2023-03-01T16:17:30.814Z',
-    lastReadDate: '2023-03-01T16:17:30.814Z',
-  },
-  {
-    id: 3,
-    title: 'News 3 title',
-    description: 'News 3 description',
-    imgUrl:
-      'https://cdn.cnn.com/cnnnext/dam/assets/230302094821-05-mv-gemini-cruise-ship-large-tease.jpg',
-    createdDate: '2023-03-01T16:17:30.814Z',
-    lastReadDate: '2023-03-02T16:17:30.814Z',
-  },
-  {
-    id: 4,
-    title: 'News 4 title',
-    description: 'News 4 description',
-    imgUrl:
-      'https://cdn.cnn.com/cnnnext/dam/assets/230302094821-05-mv-gemini-cruise-ship-large-tease.jpg',
-    createdDate: '2023-03-01T16:17:30.814Z',
-    lastReadDate: '2023-03-02T16:17:30.814Z',
-  },
-  {
-    id: 5,
-    title: 'News 5 title',
-    description: 'News 5 description',
-    imgUrl:
-      'https://cdn.cnn.com/cnnnext/dam/assets/230302094821-05-mv-gemini-cruise-ship-large-tease.jpg',
-    createdDate: '2023-03-01T16:17:30.814Z',
-    lastReadDate: '2023-03-01T16:17:30.814Z',
-  },
-  {
-    id: 6,
-    title: 'News 6 title',
-    description: 'News 6 description',
-    imgUrl:
-      'https://cdn.cnn.com/cnnnext/dam/assets/230302094821-05-mv-gemini-cruise-ship-large-tease.jpg',
-    createdDate: '2023-03-01T16:17:30.814Z',
-    lastReadDate: '2023-03-03T16:17:30.814Z',
-  },
-  {
-    id: 7,
-    title: 'News 7 title',
-    description: 'News 7 description',
-    imgUrl:
-      'https://cdn.cnn.com/cnnnext/dam/assets/230302094821-05-mv-gemini-cruise-ship-large-tease.jpg',
-    createdDate: '2023-03-01T16:17:30.814Z',
-    lastReadDate: '2023-03-03T16:17:30.814Z',
-  },
-];
-
-const lsNewsKey = 'readNews';
-localStorageService.save(lsNewsKey, newsTest);
+const lsNewsKey = 'have-read-id';
 const readNews = localStorageService.load(lsNewsKey);
-const groupedNewsByReadDate = groupByReadDate(readNews);
-renderExpansionPanel(groupedNewsByReadDate);
+if (readNews && readNews.length > 1) {
+  const groupedNewsByReadDate = groupByReadDate(readNews);
+  renderExpansionPanel(groupedNewsByReadDate);
+} else {
+  createCardNotFound();
+}
 
 function renderExpansionPanel(data) {
   const readContainer = document.querySelector('#read-container');
@@ -83,7 +23,8 @@ function renderExpansionPanel(data) {
   dates.forEach(val => {
     const panel = `<button class="accordion"><div class="accordion-date">${val}</div></button>
       <div class="panel">
-        ${renderReadNews(data[val])}
+       <ul class="news-container">
+       ${renderReadNews(data[val])}</ul>
       </div>`;
     readContainer.insertAdjacentHTML('beforeend', panel);
   });
@@ -91,11 +32,12 @@ function renderExpansionPanel(data) {
 }
 
 function renderReadNews(news) {
-  let newsLayout = '';
-  news.forEach(val => {
-    newsLayout += `<p>${val.title}</p>`;
-  });
-  return newsLayout;
+  let cards = news
+    .map(Data => {
+      return markupCard(Data);
+    })
+    .join('');
+  return cards;
 }
 
 function applyAccordion() {
@@ -119,7 +61,11 @@ function groupByReadDate(data) {
   const groups = {};
 
   data.forEach(val => {
-    const date = val.lastReadDate.split('T')[0];
+    const date = new Date(val.date)
+      .toISOString()
+      .split('T')[0]
+      .split('-')
+      .join('/');
     if (date in groups) {
       groups[date].push(val);
     } else {
