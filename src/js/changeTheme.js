194fe7587248cdd36e78b414dca60393c4192d36
Of-Nameless <79377789+Of-Nameless@users.noreply.github.com>
2023-03-08 12:39:59 +0100

    addInputColor

--- src/js/changeTheme.js
+++ src/js/changeTheme.js
@@ -10,6 +10,7 @@ const theme = {
 };
 
 const storageKey = 'themeKey';
+const toggle = document.querySelector('.theme__track');
 
 function goDark(event) {
   if (event.target.checked) {
@@ -17,12 +18,16 @@ function goDark(event) {
     refs.bodyEl.classList.add('dark-theme');
     refs.darkTheme.classList.add('goDark');
     refs.lightTheme.classList.remove('goLight');
+    toggle.classList.add('toggleDark');
+    toggle.classList.remove('toggleLight');
     localStorage.setItem(storageKey, JSON.stringify(theme.dark));
   } else {
     refs.bodyEl.classList.remove('dark-theme');
     refs.bodyEl.classList.add('ligth-theme');
     refs.darkTheme.classList.remove('goDark');
     refs.lightTheme.classList.add('goLight');
+    toggle.classList.add('toggleLight');
+    toggle.classList.remove('toggleDark');
     localStorage.setItem(storageKey, JSON.stringify(theme.light));
   }
 }
