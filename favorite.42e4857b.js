1503885bd97b7f61d233c488aa8128e7b172917d
MaksymKonin <112409870+MaksymKonin@users.noreply.github.com>
2023-03-10 05:41:59 +0000

    Deploying to gh-pages from @ MaksymKonin/project02-news@d5f85ca17fbea84de53607256bd33dc979a07804 🚀

---
+++ favorite.42e4857b.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector(".js-menu-container"),s=document.querySelector("body"),t=document.querySelector(".mobile__menu-list"),c=document.querySelector(".js-open-menu"),l=document.querySelector(".js-close-menu");t.addEventListener("click",(e=>{s.classList.remove("scroll-disable-state")})),c.addEventListener("click",(t=>{e.classList.remove("is-hidden"),s.classList.add("scroll-disable-state")})),l.addEventListener("click",(t=>{e.classList.add("is-hidden"),s.classList.remove("scroll-disable-state")}))}();
+//# sourceMappingURL=favorite.42e4857b.js.map
--- favorite.e0a90d01.js
+++
@@ -1,2 +0,0 @@
-!function(){const e=document.querySelector(".js-menu-container"),n=document.querySelector(".js-open-menu"),c=document.querySelector(".js-close-menu");n.addEventListener("click",(n=>{e.classList.remove("is-hidden")})),c.addEventListener("click",(n=>{e.classList.add("is-hidden")}))}();
-//# sourceMappingURL=favorite.e0a90d01.js.map
